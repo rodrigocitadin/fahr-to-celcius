feac83a2b261977b46fced7734f751038f233351
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-07 02:39:03 -0300

    I'm done, but I don't know if I applied the best practices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nConversor de temperatura\n");
+
+    loop {
+        println!("Converter para:");
+        println!("Celcius (C)       Fahrenheit (F)\n");
+
+        let mut escala: String = String::new();
+
+        io::stdin()
+            .read_line(&mut escala)
+            .expect("Falha ao ler entrada");
+
+        let escala: u8 = match escala.trim() {
+            "C" => 1,
+            "F" => 2,
+            _ => continue,
+        };
+
+        println!("\nInforme uma temperatura\n");
+
+        let mut temperatura: String = String::new();
+
+        io::stdin()
+            .read_line(&mut temperatura)
+            .expect("Falha ao ler entrada");
+
+        let temperatura: f64 = match temperatura.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if escala == 1 {
+            converter_para_celcius(temperatura);
+        } else {
+            converter_para_fahr(temperatura);
+        }
+    }
+}
+
+// Formula  (F − 32) × 5/9 = °C
+fn converter_para_celcius(temperatura: f64) {
+    let temperatura_convertida: f64 = (temperatura - 32.0) * 5.0 / 9.0;
+
+    println!(
+        "\n{} °F para celsius é: {} °C\n\n",
+        temperatura, temperatura_convertida
+    );
+}
+
+// Formula (C × 9/5) + 32 = °F
+fn converter_para_fahr(temperatura: f64) {
+    let temperatura_convertida: f64 = (temperatura * 9.0 / 5.0) + 32.0;
+
+    println!(
+        "\n{} °C para Fahrenheit é: {} °F\n\n",
+        temperatura, temperatura_convertida
+    );
 }
